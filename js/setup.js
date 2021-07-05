fecf482cd2136af66b1e1a5560a4d86d623ad73b
victoroliveira13 <victor12321@hotmail.com.br>
2021-07-05 18:21:32 -0300

    versao inicial

---
+++ js/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:5,
+  nav:false,
+  responsive:{
+      600:{
+          items:3
+      },
+      800: {
+          items:4
+      },
+      1100:{
+          items:5
+      }
+  }
+})
